ce103738d1033e7a927ec7f52c8fe2c4d105a4ed
Anon212212 <33222876+Anon212212@users.noreply.github.com>
2025-11-21 13:56:19 +0000

    Cleaned up networking an vairables

--- main.tf
+++ main.tf
@@ -1,4 +1,9 @@
-# Variables: org_prefix, location, hub_address_space, spoke_address_space, log_analytics_sku
+# Assumes variables:
+# - var.org_prefix
+# - var.location
+# - var.hub_address_space   = "10.145.0.0/24"
+# - var.spoke_address_space = "10.145.1.0/24"
+# - var.log_analytics_sku
 
 data "azurerm_subscription" "current" {}
 
@@ -27,21 +32,41 @@ resource "azurerm_virtual_network" "hub" {
   resource_group_name = azurerm_resource_group.connectivity.name
 }
 
-# NVA subnets (ensure these ranges fit inside var.hub_address_space)
+# Hub subnets inside 10.145.0.0/24
+
+# NVA external subnet: 10.145.0.0/27
 resource "azurerm_subnet" "hub_nva_external" {
   name                 = "nva-external-snet"
   resource_group_name  = azurerm_resource_group.connectivity.name
   virtual_network_name = azurerm_virtual_network.hub.name
-  address_prefixes     = ["10.0.1.0/27"]
+  address_prefixes     = ["10.145.0.0/27"]
 }
 
+# NVA internal subnet: 10.145.0.32/27
 resource "azurerm_subnet" "hub_nva_internal" {
   name                 = "nva-internal-snet"
   resource_group_name  = azurerm_resource_group.connectivity.name
   virtual_network_name = azurerm_virtual_network.hub.name
-  address_prefixes     = ["10.0.1.32/27"]
+  address_prefixes     = ["10.145.0.32/27"]
 }
 
+# Management subnet: 10.145.0.64/27
+resource "azurerm_subnet" "hub_mgmt" {
+  name                 = "mgmt-snet"
+  resource_group_name  = azurerm_resource_group.connectivity.name
+  virtual_network_name = azurerm_virtual_network.hub.name
+  address_prefixes     = ["10.145.0.64/27"]
+}
+
+# Azure Bastion subnet: 10.145.0.96/27 (name must be AzureBastionSubnet)
+resource "azurerm_subnet" "hub_bastion" {
+  name                 = "AzureBastionSubnet"
+  resource_group_name  = azurerm_resource_group.connectivity.name
+  virtual_network_name = azurerm_virtual_network.hub.name
+  address_prefixes     = ["10.145.0.96/27"]
+}
+
+# NSG for NVA NICs
 resource "azurerm_network_security_group" "nva" {
   name                = "${var.org_prefix}-nva-nsg"
   location            = var.location
@@ -72,6 +97,31 @@ resource "azurerm_network_security_group" "nva" {
   }
 }
 
+# NSG for management subnet
+resource "azurerm_network_security_group" "mgmt" {
+  name                = "${var.org_prefix}-mgmt-nsg"
+  location            = var.location
+  resource_group_name = azurerm_resource_group.connectivity.name
+
+  security_rule {
+    name                       = "Allow-VNet-Inbound"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "*"
+    source_port_range          = "*"
+    destination_port_range     = "*"
+    source_address_prefix      = "VirtualNetwork"
+    destination_address_prefix = "*"
+  }
+}
+
+resource "azurerm_subnet_network_security_group_association" "hub_mgmt" {
+  subnet_id                 = azurerm_subnet.hub_mgmt.id
+  network_security_group_id = azurerm_network_security_group.mgmt.id
+}
+
+# Public IP for NVA external NIC
 resource "azurerm_public_ip" "nva" {
   name                = "${var.org_prefix}-nva-pip"
   location            = var.location
@@ -80,7 +130,7 @@ resource "azurerm_public_ip" "nva" {
   sku                 = "Standard"
 }
 
-# External NIC (with public IP)
+# NVA external NIC (with public IP)
 resource "azurerm_network_interface" "nva_external" {
   name                 = "${var.org_prefix}-nva-ext-nic"
   location             = var.location
@@ -91,12 +141,12 @@ resource "azurerm_network_interface" "nva_external" {
     name                          = "external"
     subnet_id                     = azurerm_subnet.hub_nva_external.id
     private_ip_address_allocation = "Static"
-    private_ip_address            = "10.0.1.4"
+    private_ip_address            = "10.145.0.4"
     public_ip_address_id          = azurerm_public_ip.nva.id
   }
 }
 
-# Internal NIC (no public IP)
+# NVA internal NIC
 resource "azurerm_network_interface" "nva_internal" {
   name                 = "${var.org_prefix}-nva-int-nic"
   location             = var.location
@@ -107,11 +157,11 @@ resource "azurerm_network_interface" "nva_internal" {
     name                          = "internal"
     subnet_id                     = azurerm_subnet.hub_nva_internal.id
     private_ip_address_allocation = "Static"
-    private_ip_address            = "10.0.1.36"
+    private_ip_address            = "10.145.0.36"
   }
 }
 
-# Attach NSG to both NICs
+# Attach NSG to both NVA NICs
 resource "azurerm_network_interface_security_group_association" "nva_external" {
   network_interface_id      = azurerm_network_interface.nva_external.id
   network_security_group_id = azurerm_network_security_group.nva.id
@@ -156,31 +206,30 @@ resource "azurerm_linux_virtual_machine" "nva" {
   }
 }
 
-# NVA-related variables
-variable "nva_admin_username" {
-  description = "Admin username for the NVA."
-  type        = string
-  default     = "nvaadmin"
+# Azure Bastion
+
+resource "azurerm_public_ip" "bastion" {
+  name                = "${var.org_prefix}-bastion-pip"
+  location            = var.location
+  resource_group_name = azurerm_resource_group.connectivity.name
+  allocation_method   = "Static"
+  sku                 = "Standard"
 }
 
-variable "nva_admin_ssh_public_key" {
-  description = "SSH public key for the NVA admin account."
-  type        = string
-}
+resource "azurerm_bastion_host" "bastion" {
+  name                = "${var.org_prefix}-bastion"
+  location            = var.location
+  resource_group_name = azurerm_resource_group.connectivity.name
 
-variable "nva_mgmt_source_cidr" {
-  description = "CIDR allowed to SSH to the NVA."
-  type        = string
-  default     = "0.0.0.0/0"
-}
-
-variable "nva_size" {
-  description = "VM size for the NVA appliance."
-  type        = string
-  default     = "Standard_B2s"
+  ip_configuration {
+    name                 = "bastion-ip-config"
+    subnet_id            = azurerm_subnet.hub_bastion.id
+    public_ip_address_id = azurerm_public_ip.bastion.id
+  }
 }
 
 # Shared VNet and peering
+
 resource "azurerm_virtual_network" "shared" {
   name                = "${var.org_prefix}-shared-vnet"
   address_space       = [var.spoke_address_space]
@@ -204,4 +253,29 @@ resource "azurerm_virtual_network_peering" "shared_to_hub" {
   remote_virtual_network_id    = azurerm_virtual_network.hub.id
   allow_forwarded_traffic      = true
   allow_virtual_network_access = true
+}
+
+# NVA-related variables (can live in variables.tf, included here for completeness)
+
+variable "nva_admin_username" {
+  description = "Admin username for the NVA."
+  type        = string
+  default     = "nvaadmin"
+}
+
+variable "nva_admin_ssh_public_key" {
+  description = "SSH public key for the NVA admin account."
+  type        = string
+}
+
+variable "nva_mgmt_source_cidr" {
+  description = "CIDR allowed to SSH to the NVA."
+  type        = string
+  default     = "0.0.0.0/0"
+}
+
+variable "nva_size" {
+  description = "VM size for the NVA appliance."
+  type        = string
+  default     = "Standard_B2s"
 }
--- variables.tf
+++ variables.tf
@@ -1,5 +1,25 @@
-ariable "org_prefix"           { type = string }
-variable "location"             { type = string  default = "northeurope" }
-variable "hub_address_space"    { type = string  default = "10.145.0.0/24" }
-variable "spoke_address_space"  { type = string  default = "10.145.0.0/24" }
-variable "log_analytics_sku"    { type = string  default = "PerGB2018" }
+variable "org_prefix" {
+  type = string
+}
+
+variable "location" {
+  type    = string
+  default = "northeurope"
+}
+
+# Hub VNet (space for NVA + Bastion + mgmt)
+variable "hub_address_space" {
+  type    = string
+  default = "10.145.0.0/24"
+}
+
+# Spoke VNet (must NOT overlap hub)
+variable "spoke_address_space" {
+  type    = string
+  default = "10.145.1.0/24"
+}
+
+variable "log_analytics_sku" {
+  type    = string
+  default = "PerGB2018"
+}
